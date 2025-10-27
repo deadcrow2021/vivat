a090748089564c0250a2e2061065c294650ca552
deadcrow2021 <majorpayne2021@gmail.com>
2025-10-28 00:18:46 +0300

    Fix

--- app/src/application/interfaces/interactors/food_variant_interactor.py
+++ app/src/application/interfaces/interactors/food_variant_interactor.py
@@ -98,7 +98,7 @@ class GetMenuCategoryPositionsIngredientsInteractor:
                     image_url=food.image_url or "",
                     description=food.description or "",
                     measure_name=food.measure_name or "",
-                    size=sorted(size_info, key=lambda x: x['measure_value']),
+                    size=sorted(size_info, key=lambda x: x.measure_value),
                     ingredients=ingredients if ingredients else None
                 )
             ) 
